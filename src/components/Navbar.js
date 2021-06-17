8f7b08897bbb1a44a3d903bacff1907594d0d8ab
Bjørn-Magnus Fromreide <bmsf@Bjrn-Magnuss-MacBook-Pro.local>
2021-06-17 11:54:13 +0200

    Start react

---
+++ src/components/Navbar.js
@@ -0,0 +1,31 @@
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-title">BLOMSTER WERKET</div>
+      <a href="#" className="toggle-button">
+        <span className="bar"></span>
+        <span className="bar"></span>
+        <span className="bar"></span>
+      </a>
+      <div className="navbar-links">
+        <ul>
+          <li>
+            <a href="#"></a>Binderi
+          </li>
+          <li>
+            <a href="#"></a>Sesongens varer
+          </li>
+          <li>
+            <a href="#"></a>Tøffe planter
+          </li>
+          <li>
+            <a href="#"></a>Bryllup
+          </li>
+          <li>
+            <a href="#"></a>Begravelse
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
